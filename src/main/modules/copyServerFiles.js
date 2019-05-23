f68a45148e00ab0e55018022bcac52bb0bbed146
szk2s <suzukisjss@gmail.com>
2019-05-23 20:32:35 +0900

    Launch Server on loading

---
+++ src/main/modules/copyServerFiles.js
@@ -0,0 +1,51 @@
+import fs from 'fs';
+import path from 'path';
+
+const copyServerFiles = (dest) => {
+  if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest);
+  }
+  fs.copyFileSync(path.join(__static, '/server/app.py'), path.join(dest, 'app.py'));
+  copyFolderRecursiveSync(path.join(__static, '/server/venv'), dest);
+  copyFolderRecursiveSync(path.join(__static, '/server/modules'), dest);
+};
+
+export default copyServerFiles;
+
+// Define subfunctions
+
+const copyFileSync = (source, target) => {
+  let targetFile = target;
+
+  // if target is a directory a new file with the same name will be created
+  if (fs.existsSync(target)) {
+    if (fs.lstatSync(target).isDirectory()) {
+      targetFile = path.join(target, path.basename(source));
+    }
+  }
+
+  fs.writeFileSync(targetFile, fs.readFileSync(source));
+};
+
+const copyFolderRecursiveSync = (source, target) => {
+  let files = [];
+
+  // check if folder needs to be created or integrated
+  const targetFolder = path.join(target, path.basename(source));
+  if (!fs.existsSync(targetFolder)) {
+    fs.mkdirSync(targetFolder);
+  }
+
+  // copy
+  if (fs.lstatSync(source).isDirectory()) {
+    files = fs.readdirSync(source);
+    files.forEach((file) => {
+      const curSource = path.join(source, file);
+      if (fs.lstatSync(curSource).isDirectory()) {
+        copyFolderRecursiveSync(curSource, targetFolder);
+      } else {
+        copyFileSync(curSource, targetFolder);
+      }
+    });
+  }
+};
