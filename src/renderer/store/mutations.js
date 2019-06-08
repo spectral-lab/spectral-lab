728319a04cc78d9ed20ebab2f46922971e08c918
szk2s <suzukisjss@gmail.com>
2019-06-09 00:17:44 +0900

    Improve code consistency

--- src/renderer/store/mutations.js
+++ src/renderer/store/mutations.js
@@ -2,9 +2,9 @@ import { SET_SOURCE_AUDIO, SET_SPECTROGRAM } from '../constants/mutation-types';
 
 export default {
   [SET_SOURCE_AUDIO] (state, { buffer, filepath }) {
-    state.sourceAudio = Object.assign({}, {buffer, filepath});
+    state.sourceAudio = Object.assign({}, { buffer, filepath });
   },
   [SET_SPECTROGRAM] (state, { spectrogram }) {
-    state.spectrogram = spectrogram;
+    state.spectrogram = Object.assign({}, spectrogram);
   }
 };
