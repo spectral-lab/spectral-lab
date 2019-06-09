537e66bc06b61027e9cdd637b56d922c3262d612
szk2s <suzukisjss@gmail.com>
2019-06-09 23:21:26 +0900

    Implement UI
    - Implement mouse mode selector
    - Implement resizing
    - Clean up layout
    - Bundle icon library into app

--- src/renderer/components/ViewerToolbar.vue
+++ src/renderer/components/ViewerToolbar.vue
@@ -1,41 +1,60 @@
 <template>
-  <v-layout row>
-    <v-toolbar color="grey darken-3" height=160>
+  <div>
+    <v-layout row>
+      <v-toolbar color="grey darken-3" height=160>
+          <v-layout column>
+            <v-layout row class="mb-3">
+              <v-spacer />
+                Spectrogram
+              <v-spacer />
+            </v-layout>
+            <v-layout row align-center>
+              <v-flex>
+                <icon-btn-with-tip @click="buildSpectrogram" icon="build" tip="Build a spectrogram of the source audio." />
+              </v-flex>
+              <v-flex>
+                <v-btn fab color="primary" class="elevation-0">
+                  <v-icon>delete_forever</v-icon>
+                </v-btn>
+              </v-flex>
+            </v-layout>
+          </v-layout>
+      </v-toolbar>
+      <v-toolbar color="grey darken-3" height=160>
         <v-layout column>
           <v-layout row class="mb-3">
             <v-spacer />
-              Spectrogram
+              Midi
             <v-spacer />
           </v-layout>
-          <v-layout row>
-            <icon-btn-with-tip @click="buildSpectrogram" icon="build" tip="Build a spectrogram of the source audio." />
-            <v-btn fab large color="primary" class="elevation-0">
-              <v-icon>delete_forever</v-icon>
-            </v-btn>
+          <v-layout row align-center>
+            <v-flex>
+              <v-btn-toggle @change="emitMouseMode" mandatory>
+                <v-btn class="mouse-pointer">
+                  <v-icon flat>fa-mouse-pointer</v-icon>
+                </v-btn>
+                <v-btn>
+                  <v-icon flat>fa-pen</v-icon>
+                </v-btn>
+              </v-btn-toggle>
+            </v-flex>
+            <v-flex>
+              <v-btn fab color="primary" class="elevation-0">
+                <v-icon>fa-flask</v-icon>
+              </v-btn>
+            </v-flex>
           </v-layout>
         </v-layout>
-    </v-toolbar>
-    <v-toolbar color="grey darken-3" height=160>
-      <v-layout column>
-        <v-layout row class="mb-3">
-          <v-spacer />
-            Midi
-          <v-spacer />
-        </v-layout>
-        <v-layout raw>
-          <v-btn fab large color="primary" class="elevation-0">
-            <v-icon>build</v-icon>
-          </v-btn>
-        </v-layout>
-      </v-layout>
-    </v-toolbar>
-  </v-layout>
+      </v-toolbar>
+    </v-layout>
+  </div>
 </template>
 
 <script>
 import IconBtnWithTip from './IconBtnWithTip';
 import { stft, resample } from '../utils/audio';
 import { SET_SPECTROGRAM } from '../constants/mutation-types';
+import * as MOUSE_MODES from '../constants/mouse-modes';
 
 export default {
   components: {
@@ -52,7 +71,19 @@ export default {
         type: SET_SPECTROGRAM,
         spectrogram
       });
+    },
+    emitMouseMode (modeIdx) {
+      switch (modeIdx) {
+        case 0:
+          this.$emit('mousemode', MOUSE_MODES.SELECT);
+          break;
+        case 1:
+          this.$emit('mousemode', MOUSE_MODES.PEN);
+          break;
+      }
     }
   }
 };
 </script>
+
+
