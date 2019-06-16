78547ef46f97b3ae5605229c13b85f7e647ea75d
szk2s <suzukisjss@gmail.com>
2019-06-16 13:28:57 +0900

    WIP: implement OutputManager class
    - constructor
    - allocateChannel method

--- src/renderer/classes/outputManager/index.js
+++ src/renderer/classes/outputManager/index.js
@@ -1,15 +1,41 @@
+import MemberChannel from './MemberChannel';
 
 const defaults = {
-  masterChannels: 1,
+  masterChannels: [1],
   memberChannels: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
-  pitchBendRange: 48
+  pitchBendRange: 48,
+  nowCb: performance.now,
+  midiOutput: { output: console.log }
 };
 
-class outputManager {
+class OutputManager {
+  /**
+   * @param  {object} options
+   * @param  {object} options.midiOutput
+   * @param  {number} options.pitchBendRange
+   * @param  {function} options.nowCb
+   * @param  {Array.<number>} options.memberChannels
+   * @param  {Array.<number>} options.masterChannels
+   */
   constructor (options) {
-    this.options = Object.assign({}, defaults, options);
-    this.channels = [];
+    this.pitchBendRange = options.pitchBendRange;
+    this.now = options.nowCb;
+    this.midiOutput = options.midiOutput;
+    // TODO: Implement MasterChannel
+    this.memberChannels = options.memberChannels
+      .map(midiChannel => new MemberChannel({ midiChannel, nowCb: this.now }));
+  }
+  allocateChannel () {
+    const unoccupiedChannels = this.memberChannels.filter(memberChannel => !memberChannel.isOccupied());
+    if (unoccupiedChannels.length === 0) {
+      const channelWithOldestLastNoteOn = unoccupiedChannels
+        .reduce((acc, memberChannel) => memberChannel.timeOfLastNoteOn < acc.timeOfLastNoteOn ? memberChannel : acc, unoccupiedChannels[0]);
+      return channelWithOldestLastNoteOn;
+    }
+    const channelWithOldestLastNoteOff = unoccupiedChannels
+      .reduce((acc, memberChannel) => memberChannel.timeOfLastNoteOff < acc.timeOfLastNoteOff ? memberChannel : acc, unoccupiedChannels[0]);
+    return channelWithOldestLastNoteOff;
   }
 }
 
-export default outputManager;
+export default OutputManager;
