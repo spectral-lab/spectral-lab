55968b252b9b0ff37770d9994913af539a45b928
szk2s <suzukisjss@gmail.com>
2019-06-17 21:59:32 +0900

    Implement NoteFactory

--- src/renderer/classes/NoteFactory.js
+++ src/renderer/classes/NoteFactory.js
@@ -1,24 +1,56 @@
-import { NOTE_ON } from '../constants/defaults';
+import * as defaults from '../constants/defaults';
+import { NOTE_ON } from '../constants/note-action-types';
 import { pick } from 'lodash';
 import '../typedef';
-const noteOnProperties = ['time', 'pitch', 'pressure', 'timbre', 'noteOnVelocity'];
+
+const noteOnProperties = Object.keys(defaults.noteOn);
 
 class NoteFactory {
   constructor () {
     this.id = 0;
   }
   /**
-   * @param  {NoteOn} noteOn
-   * @returns {{id: number, noteOn: object, modulations: array, duration: number | null}}
+   * @param {object} materials
+   * @returns {Note}
    */
-  createNote (noteOn) {
+  createNote (materials) {
+    // pitch = noteNumber + pitchBend
+    if (materials.time != null && materials.pitch != null) return this.noteFromPitch(materials);
+    if (materials.time != null && materials.noteNumber != null) return this.noteFromNoteNumber(materials);
+    throw new Error('To create note, either (time and pitch) or (time and noteNumber) is necessary');
+  }
+  noteFromPitch (materials) {
+    const noteNumber = Math.round(materials.pitch);
+    const pitchBend = materials.pitch - noteNumber;
+    const noteOn = Object.assign(
+      {},
+      defaults.noteOn,
+      pick(materials, noteOnProperties),
+      { noteNumber, pitchBend },
+      { type: NOTE_ON }
+    );
     const note = {
       id: this.id,
-      noteOn: Object.assign({}, NOTE_ON, pick(noteOn, noteOnProperties)),
-      modulations: [],
-      duration: null
+      noteOn,
+      noteOff: null,
+      modulations: []
     };
     this.id++;
+    return note;
+  }
+  noteFromNoteNumber (materials) {
+    const noteOn = Object.assign(
+      {},
+      defaults.noteOn,
+      pick(materials, noteOnProperties),
+      { type: NOTE_ON }
+    );
+    const note = {
+      id: this.id,
+      noteOff: null,
+      noteOn,
+      modulations: []
+    };
     return note;
   }
 }
