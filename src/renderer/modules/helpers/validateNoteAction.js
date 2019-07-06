7392459512ec33b3d60d77a5b98c7199d9a39dc8
Satoshi Suzuki <31060964+szk2s@users.noreply.github.com>
2019-07-06 22:33:05 +0900

    Implement new action `ADD_NOTE` (#51)

--- src/renderer/modules/helpers/validateNoteAction.js
+++ src/renderer/modules/helpers/validateNoteAction.js
@@ -1,5 +1,5 @@
 export const validateModulation = (mod) => {
-  if (mod.offsetTime != null) {
+  if (mod.offsetTime != null && mod.type === 'MODULATION') {
     if (mod.pitchBend != null) return;
     if (mod.timbre != null) return;
     if (mod.pressure != null) return;
@@ -8,8 +8,10 @@ export const validateModulation = (mod) => {
 };
 
 export const validateNoteOff = (noteOff) => {
-  if (noteOff.offsetTime != null) {
-    if (noteOff.noteOffVelocity != null) return;
-  };
-  throw new Error('Invalid modulation');
+  if (
+    noteOff.offsetTime != null &&
+    noteOff.type === 'NOTE_OFF' &&
+    noteOff.noteOffVelocity != null
+  ) return;
+  throw new Error('Invalid noteOff');
 };
--- src/renderer/store/action-types.js
+++ src/renderer/store/action-types.js
@@ -1,4 +1,5 @@
 export const ACCEPT_AUDIO = 'ACCEPT_AUDIO';
+export const ADD_NOTE = 'ADD_NOTE';
 export const MODULATE_NOTE = 'MODULATE_NOTE';
 export const CREATE_NOTE = 'CREATE_NOTE';
 export const RELEASE_NOTE = 'RELEASE_NOTE';
