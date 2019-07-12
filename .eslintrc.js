f9ebdec855f65acfe86cc09b26fbdbf668cb47af
szk2s <suzukisjss@gmail.com>
2019-07-12 19:26:32 +0900

    Modify linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,7 @@ module.exports = {
     'no-trailing-spaces': process.env.NODE_ENV === 'production' ? 2 : 1,
     'object-curly-spacing': process.env.NODE_ENV === 'production' ? ["error", "always"] : ["warn", "always"],
     'comma-spacing': process.env.NODE_ENV === 'production' ? 2 : 1,
-    'space-before-function-paren': process.env.NODE_ENV === 'production' ? 2 : 1
+    'space-before-function-paren': process.env.NODE_ENV === 'production' ? 2 : 1,
+    'comma-dangle': process.env.NODE_ENV === 'production' ? 2 : 1
   }
 }
