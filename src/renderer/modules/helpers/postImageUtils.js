9532b48ec1f4b223be77a9e42838770ccead2135
szk2s <suzukisjss@gmail.com>
2019-07-20 11:25:30 +0900

    Derive noteOff pitchBend and pressure when extracting notes from spectrogram

--- src/renderer/modules/helpers/postImageUtils.js
+++ src/renderer/modules/helpers/postImageUtils.js
@@ -74,9 +74,10 @@ export const parsePointAsNoteOn = (noteOnPoint, spectrogram, secToTick) => {
  * @param  {Array.<number>} modulationPoint
  * @param  {object} spectrogram
  * @param  {function} secToTick
- * @param  {number} noteOnNoteNumber
+ * @param  {number} noteOnTime
+ * @param  {number} noteNumber
  */
-export const parsePointAsModulation = (modulationPoint, spectrogram, secToTick, noteOnTime, noteOnNoteNumber) => {
+export const parsePointAsModulation = (modulationPoint, spectrogram, secToTick, noteOnTime, noteNumber) => {
   const timeIdx = modulationPoint[0]; // float
   const timeInterval = spectrogram.times[1] - spectrogram.times[0];
   const time = secToTick(spectrogram.times[Math.floor(timeIdx)] + timeInterval * (timeIdx - Math.floor(timeIdx)));
@@ -87,7 +88,7 @@ export const parsePointAsModulation = (modulationPoint, spectrogram, secToTick, 
   return {
     id: uid(),
     offsetTime: time - noteOnTime,
-    pitchBend: pitchAndNoteNumberToPitchBend(pitch, noteOnNoteNumber),
+    pitchBend: pitchAndNoteNumberToPitchBend(pitch, noteNumber),
     pressure: decibelCurve(modulationPoint[2])
   };
 };
@@ -96,14 +97,22 @@ export const parsePointAsModulation = (modulationPoint, spectrogram, secToTick, 
  * @param  {Array.<number>} noteOffPoint
  * @param  {object} spectrogram
  * @param  {function} secToTick
+ * @param  {number} noteOnTime
+ * @param  {number} noteNumber
  */
-export const parsePointAsNoteOff = (noteOffPoint, spectrogram, secToTick, noteOnTime) => {
+export const parsePointAsNoteOff = (noteOffPoint, spectrogram, secToTick, noteOnTime, noteNumber) => {
   const timeIdx = noteOffPoint[0]; // float
   const timeInterval = spectrogram.times[1] - spectrogram.times[0];
   const time = secToTick(spectrogram.times[Math.floor(timeIdx)] + timeInterval * (timeIdx - Math.floor(timeIdx)));
+  const freqIdx = noteOffPoint[1]; // float
+  const freqInterval = spectrogram.freqs[1] - spectrogram.freqs[0];
+  const freq = spectrogram.freqs[Math.floor(freqIdx)] + freqInterval * (freqIdx - Math.floor(freqIdx));
+  const pitch = ftom(freq);
   return {
     id: uid(),
     offsetTime: time - noteOnTime,
-    noteOffVelocity: decibelCurve(noteOffPoint[2])
+    noteOffVelocity: decibelCurve(noteOffPoint[2]),
+    pitchBend: pitchAndNoteNumberToPitchBend(pitch, noteNumber),
+    pressure: decibelCurve(noteOffPoint[2])
   };
 };
