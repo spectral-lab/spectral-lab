b413ecb7ce2a9fcdde0ae1f59ffd554b6be56202
Satoshi Suzuki <31060964+szk2s@users.noreply.github.com>
2019-08-18 19:12:41 +0900

    Set up storybook (#104)

---
+++ src/stories/other.stories.js
@@ -0,0 +1,14 @@
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+import ElasticDivStack from '../renderer/components/ElasticDivStack';
+
+storiesOf('ElasticDivStack', module)
+  .add('with Hello World', () => ({
+    components: { ElasticDivStack },
+    template: `
+      <elastic-div-stack>
+        <template #upper>Hello</template>
+        <template #lower>World</template>
+      </elastic-div-stack>`,
+    methods: { action: action('clicked') }
+  }));
