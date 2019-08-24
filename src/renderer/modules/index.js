1d1c715552076a77dcc9cd7517427ae140285d12
szk2s <suzukisjss@gmail.com>
2019-08-24 23:42:39 +0900

    refactor($view): apply new API of MIDI playback

---
+++ src/renderer/modules/index.js
@@ -0,0 +1,10 @@
+// @flow
+import { MidiPlayer } from './helpers/MidiPlayer';
+import { OutputManager } from './outputManager';
+import { MidiIoFacade } from './helpers/MidiIoFacade';
+import { Scheduler } from './helpers/Scheduler';
+
+const midiIoFacade = new MidiIoFacade(navigator);
+const outputManager = new OutputManager(midiIoFacade);
+const scheduler = new Scheduler(outputManager, () => window.performance.now());
+export const midiPlayer = new MidiPlayer(scheduler);
