86eb77e08aa1d572d5696c55c9260bbd896ac37d
szk2s <suzukisjss@gmail.com>
2019-08-24 21:12:12 +0900

    feat($core): implement MidiPlayer and Scheduler

--- src/renderer/modules/helpers/MidiPlayer.js
+++ src/renderer/modules/helpers/MidiPlayer.js
@@ -1,55 +1,40 @@
 // @flow
-import { IOutputManager } from '../outputManager';// eslint-disable-next-line no-unused-vars
-import type { NoteAction } from '../../../types';
-import { NOTE_ON, MODULATION, NOTE_OFF, CLIP, NOTE } from '../../../constants/model-types';
+import { IScheduler } from './Scheduler';
+import { CLIP, NOTE } from '../../../constants/model-types';
 // eslint-disable-next-line no-unused-vars
 import { Clip, Note } from '../../store/models';
+import { allNotesOff } from './allNotesOff';
 // eslint-disable-next-line no-unused-vars
 
-export class MidiPlayer {
-  outputManager: IOutputManager;
+export interface IMidiPlayer {
+  play(item: Clip | Note): void;
+  testTone(): void;
+  allNotesOff(): void;
+}
 
-  constructor (outputManager: IOutputManager) {
-    this.outputManager = outputManager;
+export class MidiPlayer implements IMidiPlayer {
+  scheduler: IScheduler;
+
+  constructor (scheduler: IScheduler) {
+    this.scheduler = scheduler;
   }
 
   play (item: Clip | Note) {
     switch (item.type) {
-      case CLIP: return this.playClip(item);
-      case NOTE: return this.playNote(item);
+      case CLIP: return this.scheduler.playClip(item);
+      case NOTE: return this.scheduler.playNote(item);
     }
   }
 
   testTone () {
     const noteOnMessage = [0x91, 69, 0x7f];
     const noteOffMessage = [0x81, 69, 0x40];
-    this.outputManager.send(noteOnMessage);
-    this.outputManager.send(noteOffMessage, 480);
+    this.scheduler.immediate(noteOnMessage);
+    this.scheduler.addCue(noteOffMessage, 480);
   }
 
-  playClip (clip: Clip): void {
-    const noteActions = clip.sortedNoteActions;
-    const noteControl = {};
-    noteActions.forEach(noteAction => {
-      const noteOffsetTime = noteAction.parent.offsetTime;
-      if (noteAction.type === NOTE_ON) {
-        noteControl[noteAction.noteId] = this.outputManager.noteOn(noteAction, noteOffsetTime);
-      }
-      if (noteAction.type === MODULATION) {
-        const nc = noteControl[noteAction.noteId];
-        if (!nc) return;
-        nc.modulate(noteAction, noteOffsetTime + noteAction.offsetTime);
-      }
-      if (noteAction.type === NOTE_OFF) {
-        const nc = noteControl[noteAction.noteId];
-        if (!nc) return;
-        nc.noteOff(noteAction, noteOffsetTime + noteAction.offsetTime);
-      }
-    });
-  }
-
-  playNote (note: Note): void {
-    console.log('Playing note');
+  allNotesOff () {
+    allNotesOff(message => this.scheduler.immediate(message));
   }
 }
 
---
+++ src/renderer/modules/helpers/Scheduler.js
@@ -0,0 +1,62 @@
+// @flow
+import type { IOutputManager } from '../outputManager';
+import { Clip, Note } from '../../store/models';
+import { MODULATION, NOTE_OFF, NOTE_ON } from '../../../constants/model-types';
+import type { MidiMessage, Now } from '../../../types';
+import { timeConverter } from './timeUtils';
+
+export interface IScheduler {
+  immediate(midiMessage: MidiMessage): void;
+  addCue(midiMessage: MidiMessage, tick: number): void;
+  playClip(clip: Clip): void;
+  playNote(note: Note): void;
+}
+
+export class Scheduler implements IScheduler {
+  _outputManager: IOutputManager | null;
+
+  _now: Now;
+
+  constructor (outputManager: IOutputManager | null = null, now: Now) {
+    this._outputManager = outputManager;
+    this._now = now;
+  }
+
+  immediate (midiMessage: MidiMessage) {
+    if (!this._outputManager) return;
+    this._outputManager.send(midiMessage);
+  }
+
+  addCue (midiMessage: MidiMessage, tick: number) {
+    if (!this._outputManager) return;
+    this._outputManager.send(midiMessage, this._now() + timeConverter.toMs(tick));
+  }
+
+  playClip (clip: Clip): void {
+    if (!this._outputManager) throw new Error('outputManager is not set');
+    const noteActions = clip.sortedNoteActions;
+    const noteControl = {};
+    noteActions.forEach(noteAction => {
+      const noteOffsetTime = noteAction.parent.offsetTime;
+      if (noteAction.type === NOTE_ON) {
+        if (!this._outputManager) return;
+        noteControl[noteAction.noteId] = this._outputManager.noteOn(noteAction, noteOffsetTime);
+      }
+      if (noteAction.type === MODULATION) {
+        const nc = noteControl[noteAction.noteId];
+        if (!nc) return;
+        nc.modulate(noteAction, noteOffsetTime + noteAction.offsetTime);
+      }
+      if (noteAction.type === NOTE_OFF) {
+        const nc = noteControl[noteAction.noteId];
+        if (!nc) return;
+        nc.noteOff(noteAction, noteOffsetTime + noteAction.offsetTime);
+      }
+    });
+  }
+
+  playNote (note: Note): void {
+    // TODO
+    console.log('Playing note', note);
+  }
+}
