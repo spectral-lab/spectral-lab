c35909bd98194b040ebe85bfc039f6b1479eecf6
szk2s <suzukisjss@gmail.com>
2019-09-08 19:39:31 +0900

    feat($view): improve design at TrackHeaderPanel

--- src/renderer/components/arrangement-zone/TrackHeaderPanel.vue
+++ src/renderer/components/arrangement-zone/TrackHeaderPanel.vue
@@ -1,26 +1,31 @@
 <template>
-  <v-card
-    :color="bgColor"
-    class="track-header-panel"
-    flat
-  >
-    <v-card-title primary-title>
-      <h3>
-        {{ idx + 1 }} {{ track.name }}
-      </h3>
-    </v-card-title>
-    <v-card-actions>
-      <v-layout row>
-        <v-spacer />
-        <icon-btn-with-tip
-          @click="handleClick"
-          :color="bgColor"
-          icon="fa-cog"
-          tip="Tack setting"
-        />
-      </v-layout>
-    </v-card-actions>
-  </v-card>
+  <div class="track-header-panel">
+    <div
+      :style="{backgroundColor: labelColor}"
+      class="color-label"
+    />
+    <div
+      :style="{backgroundColor: bgColor}"
+      class="panel-content"
+    >
+      <v-card-title>
+        <h3>
+          {{ idx + 1 }}. {{ track.name }}
+        </h3>
+      </v-card-title>
+      <v-card-actions>
+        <v-layout row>
+          <v-spacer />
+          <icon-btn-with-tip
+            @click="handleClick"
+            :color="bgColor"
+            icon="fa-cog"
+            tip="Tack setting"
+          />
+        </v-layout>
+      </v-card-actions>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -45,8 +50,11 @@ export default Vue.extend({
     }
   },
   computed: {
-    bgColor () {
+    labelColor () {
       return Color(this.track.color).darken(0.5).rgb().string();
+    },
+    bgColor () {
+      return 'rgb(30,30,30)';
     }
   },
   methods: {
@@ -62,5 +70,17 @@ export default Vue.extend({
 </script>
 
 <style scoped>
-
+  .track-header-panel {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+  }
+  .color-label {
+    width: 25px;
+    height: 100%;
+  }
+  .panel-content {
+    height: 100%;
+    flex: 1 10 auto;
+  }
 </style>
