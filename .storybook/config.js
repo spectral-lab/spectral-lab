0a5470c7968b54c6c3ed4aa6f1266523a90cd76d
szk2s <suzukisjss@gmail.com>
2019-09-08 11:20:00 +0900

    chore($dev): enable darkmode in storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,11 @@
-import { addDecorator, configure } from '@storybook/vue';
+import { addDecorator, addParameters, configure } from '@storybook/vue';
 import Vue from "vue";
 import Vuetify from "vuetify";
 import { theme } from '../src/constants/theme';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
+import 'vuetify/dist/vuetify.min.css';
+import { themes } from '@storybook/theming';
 
 Vue.use(Vuetify, {
   theme, iconfont: "md"
@@ -13,6 +14,12 @@ Vue.use(Vuetify, {
 addDecorator(() => ({
   template: "<v-app><story/></v-app>"
 }));
+
+addParameters({
+  options: {
+    theme: themes.dark,
+  },
+});
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../src/stories', true, /\.stories\.js$/);
