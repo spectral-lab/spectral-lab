be6c91a26ef996f88ca253dcbb663f7124f124ce
szk2s <suzukisjss@gmail.com>
2019-09-12 00:34:15 +0900

    refactor($models): remove unused property
    - at Track model

--- src/renderer/store/models/Track.js
+++ src/renderer/store/models/Track.js
@@ -17,7 +17,6 @@ export default class Track extends BaseModel {
       name: this.string('Track'),
       color: this.attr(() => SCALE_COLORS.aScriabin[random(11)]),
       type: this.string(TRACK),
-      speed: this.attr([{ offsetTime: 0, val: 1 }]),
       selected: this.boolean(false),
       songId: this.attr(null, makeMandatory('songId')),
       clips: this.hasMany(Clip, 'trackId')
