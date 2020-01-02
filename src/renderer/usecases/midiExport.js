c8f6a6db1490b8a03d5735de85dc3a55a9c8fe6b
szk2s <suzukisjss@gmail.com>
2020-01-02 22:25:09 +0900

    fix midi export

--- src/renderer/usecases/midiExport.js
+++ src/renderer/usecases/midiExport.js
@@ -5,12 +5,12 @@ import { midiWriter } from '../modules/container';
 const { dialog } = remote;
 
 export const exportSelectedClips = async (): Promise<void> => {
-  const exportDirs = await dialog.showOpenDialog({
+  const { filePaths } = await dialog.showOpenDialog({
     message: 'Choose a directory where you export',
     properties: ['openDirectory', 'createDirectory']
   });
-  if (!exportDirs || exportDirs === []) return;
+  if (filePaths === []) return;
   const selectedClips = Clip.query().where('selected', true).withAllRecursive().get();
-  await Promise.all(selectedClips.map(clip => midiWriter.exportClip(clip, exportDirs[0])));
+  await Promise.all(selectedClips.map(clip => midiWriter.exportClip(clip, filePaths[0])));
   console.log('All clips are exported successfully');
 };
