2defe8b0f94c36e7236a8ecbb727124d32841563
szk2s <suzukisjss@gmail.com>
2020-01-02 21:36:24 +0900

    refactor loadMockentities

--- src/renderer/usecases/initialize/loadMockEntities.js
+++ src/renderer/usecases/initialize/loadMockEntities.js
@@ -1,7 +1,5 @@
-import { REPLACE_ENTITIES } from '../../store/mutation-types';
-import mockEntities from '../../../../test/data/json/mockEntities';
-import { getStore } from '../../store';
+import { replaceEntitiesByProjectFile } from '../project';
 
 export const loadMockEntities = () => {
-  getStore().commit(REPLACE_ENTITIES, mockEntities);
+  replaceEntitiesByProjectFile('./test/data/json/mockProjectFile.json');
 };
