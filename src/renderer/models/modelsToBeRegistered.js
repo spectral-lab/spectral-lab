a7c959d4b3e6493e71e2d4b234ede6af449cd34a
szk2s <suzukisjss@gmail.com>
2020-01-02 13:32:44 +0900

    refactor initDatabase

---
+++ src/renderer/models/modelsToBeRegistered.js
@@ -0,0 +1,19 @@
+// @flow
+
+export {
+  AudioBuffer,
+  App,
+  Arrangement,
+  Bar,
+  Beat,
+  Clip,
+  Dialog,
+  Modulation,
+  Note,
+  NoteOff,
+  NoteOn,
+  PianoRoll,
+  Song,
+  Spectrogram,
+  Track
+} from './index';
